d1a48fe99e0d62ea5ba667de5207fb1758c419d9
baijiawei <baijiawei@onething.net>
2021-05-21 11:58:24 +0800

    求解最长公共前缀两种方法

--- 具体问题/字符串/最长公共前缀.ts
+++ 具体问题/字符串/最长公共前缀.ts
@@ -47,4 +47,26 @@ function curLongestCommonPrefix(strs: string[]) {
 }
 
 const arrs = ["flower", "flow", "flight"]
-console.log(curLongestCommonPrefix(arrs))
+console.log(curLongestCommonPrefix(arrs)) // fl
+
+
+// 解法 2 使用分制的思想  利用递归去实现
+
+// 求解两个字符串的公共前缀
+function prefixTwo(str1: string, str2: string) {
+  let j = 0
+  for (; j < str1.length && j < str1.length; j++) {
+    if (str1.charCodeAt(j) !== str2.charCodeAt(j)) break
+  }
+  return str1.substring(0, j)
+}
+
+function getCommonPrefix(arr: string[]) {
+  if (arr.length === 1) return arr[0]
+  const mid = Math.floor(arr.length / 2)
+  const leftSub = arr.slice(0, mid)
+  const rightSub = arr.slice(mid, arr.length)
+  return prefixTwo(getCommonPrefix(leftSub), getCommonPrefix(rightSub))
+}
+
+console.log(getCommonPrefix(arrs)) // fl
